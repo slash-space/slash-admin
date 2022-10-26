ce7a7a1355f0966a1a610af67c46d9860f12fcc3
waltcow <waltcow@gmail.com>
2022-10-26 23:35:40 +0800

    update menu init data

--- app/admin/cmd/api/internal/globalkey/cacheKey.go
+++ app/admin/cmd/api/internal/globalkey/cacheKey.go
@@ -1,10 +1,7 @@
 package globalkey
 
-// InitDatabaseLock init_database_lock
-const InitDatabaseLock = "init_database_lock"
-
-// InitDatabaseState
-const InitDatabaseState = "init_database_state"
-
-// InitDatabaseErrorMsg
-const InitDatabaseErrorMsg = "init_database_error_msg"
+const (
+	InitDatabaseLock     = "init_database_lock"
+	InitDatabaseState    = "init_database_state"
+	InitDatabaseErrorMsg = "init_database_error_msg"
+)
